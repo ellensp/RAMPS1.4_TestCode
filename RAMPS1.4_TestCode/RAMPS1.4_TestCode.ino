dbbc6f77efecab80d5c64db4dc6fef857eaceb69
ellensp <530024+ellensp@users.noreply.github.com>
2023-10-21 03:30:57 +1300

    reformat code

--- RAMPS1.4_TestCode/RAMPS1.4_TestCode.ino
+++ RAMPS1.4_TestCode/RAMPS1.4_TestCode.ino
@@ -28,60 +28,55 @@
 #define SDSS               53
 #define LED_PIN            13
 
-#define FAN_PIN            9
+#define FAN_PIN             9
 
 #define PS_ON_PIN          12
 #define KILL_PIN           -1
 
 #define HEATER_0_PIN       10
-#define HEATER_1_PIN       8
-#define TEMP_0_PIN          13   // ANALOG NUMBERING
-#define TEMP_1_PIN          14   // ANALOG NUMBERING
+#define HEATER_1_PIN        8
+#define TEMP_0_PIN         13   // ANALOG NUMBERING
+#define TEMP_1_PIN         14   // ANALOG NUMBERING
 
 void setup() {
-  pinMode(FAN_PIN , OUTPUT);
-  pinMode(HEATER_0_PIN , OUTPUT);
-  pinMode(HEATER_1_PIN , OUTPUT);
-  pinMode(LED_PIN  , OUTPUT);
-  
+  pinMode(FAN_PIN     , OUTPUT);
+  pinMode(HEATER_0_PIN, OUTPUT);
+  pinMode(HEATER_1_PIN, OUTPUT);
+  pinMode(LED_PIN     , OUTPUT);
+
   pinMode(X_STEP_PIN  , OUTPUT);
-  pinMode(X_DIR_PIN    , OUTPUT);
-  pinMode(X_ENABLE_PIN    , OUTPUT);
-  
+  pinMode(X_DIR_PIN   , OUTPUT);
+  pinMode(X_ENABLE_PIN, OUTPUT);
+
   pinMode(Y_STEP_PIN  , OUTPUT);
-  pinMode(Y_DIR_PIN    , OUTPUT);
-  pinMode(Y_ENABLE_PIN    , OUTPUT);
-  
+  pinMode(Y_DIR_PIN   , OUTPUT);
+  pinMode(Y_ENABLE_PIN, OUTPUT);
+
   pinMode(Z_STEP_PIN  , OUTPUT);
-  pinMode(Z_DIR_PIN    , OUTPUT);
-  pinMode(Z_ENABLE_PIN    , OUTPUT);
-  
+  pinMode(Z_DIR_PIN   , OUTPUT);
+  pinMode(Z_ENABLE_PIN, OUTPUT);
+
   pinMode(E_STEP_PIN  , OUTPUT);
-  pinMode(E_DIR_PIN    , OUTPUT);
-  pinMode(E_ENABLE_PIN    , OUTPUT);
-  
+  pinMode(E_DIR_PIN   , OUTPUT);
+  pinMode(E_ENABLE_PIN, OUTPUT);
+
   pinMode(Q_STEP_PIN  , OUTPUT);
-  pinMode(Q_DIR_PIN    , OUTPUT);
-  pinMode(Q_ENABLE_PIN    , OUTPUT);
-  
-   digitalWrite(X_ENABLE_PIN    , LOW);
-    digitalWrite(Y_ENABLE_PIN    , LOW);
-    digitalWrite(Z_ENABLE_PIN    , LOW);
-    digitalWrite(E_ENABLE_PIN    , LOW);
-    digitalWrite(Q_ENABLE_PIN    , LOW);
+  pinMode(Q_DIR_PIN   , OUTPUT);
+  pinMode(Q_ENABLE_PIN, OUTPUT);
+
+  digitalWrite(X_ENABLE_PIN, LOW);
+  digitalWrite(Y_ENABLE_PIN, LOW);
+  digitalWrite(Z_ENABLE_PIN, LOW);
+  digitalWrite(E_ENABLE_PIN, LOW);
+  digitalWrite(Q_ENABLE_PIN, LOW);
 }
 
-
-
-
-
 void loop () {
-  
-  if (millis() %1000 <500) 
+  if (millis() %1000 <500)
     digitalWrite(LED_PIN, HIGH);
   else
    digitalWrite(LED_PIN, LOW);
-  
+
   if (millis() %1000 <300) {
     digitalWrite(HEATER_0_PIN, HIGH);
     digitalWrite(HEATER_1_PIN, LOW);
@@ -95,34 +90,33 @@ void loop () {
     digitalWrite(HEATER_1_PIN, LOW);
     digitalWrite(FAN_PIN, HIGH);
   }
-  
+
   if (millis() %10000 <5000) {
-    digitalWrite(X_DIR_PIN    , HIGH);
-    digitalWrite(Y_DIR_PIN    , HIGH);
-    digitalWrite(Z_DIR_PIN    , HIGH);
-    digitalWrite(E_DIR_PIN    , HIGH);
-    digitalWrite(Q_DIR_PIN    , HIGH);
+    digitalWrite(X_DIR_PIN, HIGH);
+    digitalWrite(Y_DIR_PIN, HIGH);
+    digitalWrite(Z_DIR_PIN, HIGH);
+    digitalWrite(E_DIR_PIN, HIGH);
+    digitalWrite(Q_DIR_PIN, HIGH);
   }
   else {
-    digitalWrite(X_DIR_PIN    , LOW);
-    digitalWrite(Y_DIR_PIN    , LOW);
-    digitalWrite(Z_DIR_PIN    , LOW);
-    digitalWrite(E_DIR_PIN    , LOW);
-    digitalWrite(Q_DIR_PIN    , LOW);
+    digitalWrite(X_DIR_PIN, LOW);
+    digitalWrite(Y_DIR_PIN, LOW);
+    digitalWrite(Z_DIR_PIN, LOW);
+    digitalWrite(E_DIR_PIN, LOW);
+    digitalWrite(Q_DIR_PIN, LOW);
   }
-  
-  
-    digitalWrite(X_STEP_PIN    , HIGH);
-    digitalWrite(Y_STEP_PIN    , HIGH);
-    digitalWrite(Z_STEP_PIN    , HIGH);
-    digitalWrite(E_STEP_PIN    , HIGH);
-    digitalWrite(Q_STEP_PIN    , HIGH); 
+
+  digitalWrite(X_STEP_PIN, HIGH);
+  digitalWrite(Y_STEP_PIN, HIGH);
+  digitalWrite(Z_STEP_PIN, HIGH);
+  digitalWrite(E_STEP_PIN, HIGH);
+  digitalWrite(Q_STEP_PIN, HIGH);
+
   delay(1);
-    
-    digitalWrite(X_STEP_PIN    , LOW);
-    digitalWrite(Y_STEP_PIN    , LOW);
-    digitalWrite(Z_STEP_PIN    , LOW);
-    digitalWrite(E_STEP_PIN    , LOW);
-    digitalWrite(Q_STEP_PIN    , LOW); 
-  
+
+  digitalWrite(X_STEP_PIN, LOW);
+  digitalWrite(Y_STEP_PIN, LOW);
+  digitalWrite(Z_STEP_PIN, LOW);
+  digitalWrite(E_STEP_PIN, LOW);
+  digitalWrite(Q_STEP_PIN, LOW);
 }
